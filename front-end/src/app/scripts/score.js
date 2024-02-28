83bf633ad52a16a5234f3556701c0af9385dfa07
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-02-28 15:58:08 +0100

    Passage des classes en ES6

--- front-end/src/app/scripts/score.js
+++ front-end/src/app/scripts/score.js
@@ -1,24 +1,25 @@
 import template from "../views/score.html";
+import { Component } from "./component";
 
 import { parseUrl } from "./utils.js";
 
-// TODO #class: use the ES6 class keyword
 /* class ScoreComponent constructor */
-export function ScoreComponent() {
-  // TODO #extends: call super(template)
-  var params = parseUrl();
+export class ScoreComponent extends Component {
+  constructor() {
+    super(template)
+    var params = parseUrl();
 
-  this.template = template;
-  this.name = params.name;
-  this.size = parseInt(params.size);
-  this.time = parseInt(params.time);
+    this.template = template;
+    this.name = params.name;
+    this.size = parseInt(params.size);
+    this.time = parseInt(params.time);
+  }
+
+  /* method ScoreComponent.init */
+  init() {
+    document.getElementById("name").innerText = this.name;
+    document.getElementById("size").innerText = this.size;
+    document.getElementById("time").innerText = this.time;
+  };
 }
-
-// TODO #class: turn function into a method of ScoreComponent
-/* method ScoreComponent.init */
-ScoreComponent.prototype.init = function init() {
-  document.getElementById("name").innerText = this.name;
-  document.getElementById("size").innerText = this.size;
-  document.getElementById("time").innerText = this.time;
-};
 
