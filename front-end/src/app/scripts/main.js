33fd8f586d328f93457b381293fa6dd703114d21
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-02-28 14:59:38 +0100

    CSS Loader

--- front-end/src/app/scripts/main.js
+++ front-end/src/app/scripts/main.js
@@ -4,6 +4,9 @@ import { GameComponent } from "./game.js";
 import { ScoreComponent } from "./score.js";
 // TODO #import-css: use ES side-effect imports to import styles/style.css
 
+import '/node_modules/bootstrap/dist/css/bootstrap.css';
+import "../styles/style.css";
+
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
 router
