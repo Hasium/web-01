690b8fa4b0cd52f0108f5d32ba469c8e22aee21d
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-03-05 11:13:14 +0100

    Création et utilisation des composants navbar et footer

--- front-end/src/app/scripts/main.js
+++ front-end/src/app/scripts/main.js
@@ -2,10 +2,15 @@ import { Router } from "./router.js";
 import { WelcomeComponent } from "../components/welcome/welcome.component.js";
 import { GameComponent } from "../components/game/game.component.js";
 import { ScoreComponent } from "../components/score/score.component.js";
-// TODO #import-css: use ES side-effect imports to import styles/style.css
+
+import { NavbarComponent } from "../components/navbar/navbar.component.js";
+import { FooterComponent } from "../components/footer/footer.compenent.js";
 
 import '/node_modules/bootstrap/dist/css/bootstrap.css';
 import "../styles/style.css";
+
+customElements.define("my-navbar", NavbarComponent);
+customElements.define("my-footer", FooterComponent);
 
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
