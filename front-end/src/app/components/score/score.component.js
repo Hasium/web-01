f55542724f31ddf2a0f00533ab4bd01373b48a7a
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-03-20 09:50:36 +0100

    BONNUS Ajout d'un filtre sur le tableau des scores

--- front-end/src/app/components/score/score.component.js
+++ front-end/src/app/components/score/score.component.js
@@ -17,6 +17,7 @@ export class ScoreComponent extends Component {
     this.name = params.name;
     this.size = parseInt(params.size);
     this.time = parseInt(params.time);
+    this.scores = [];
   }
 
   /* method ScoreComponent.init */
@@ -24,9 +25,31 @@ export class ScoreComponent extends Component {
     document.getElementById("name").innerText = this.name;
     document.getElementById("size").innerText = this.size;
     document.getElementById("time").innerText = this.time;
-    let scores = await this._getScores();
-    scores = scores.sort((a, b) => a.time - b.time);
-    this._displayScores(scores);
+    let scores_temp = await this._getScores();
+    this.scores = scores_temp.sort((a, b) => a.time - b.time)
+      .sort((a, b) => a.size - b.size)
+    const sizes = [...new Set(this.scores.map(score => score.size))]
+    this._displayScores(this.scores);
+
+    const dropdown = document.getElementsByClassName("dropdown-menu")
+    const option = document.createElement("div")
+    option.classList.add("dropdown-element")
+    option.innerText = `Toutes les paires`
+    option.onclick = () => {
+      this._displayScores(this.scores)
+    }
+    dropdown[0].appendChild(option)
+
+    sizes.forEach(size => {
+      const option = document.createElement("div")
+      option.classList.add("dropdown-element")
+      option.innerText = `${size} paires`
+      option.onclick = () => {
+        let scores = this.scores.filter(score => score.size === size).sort((a, b) => a.time - b.time)
+        this._displayScores(scores)
+      }
+      dropdown[0].appendChild(option)
+    })
   };
 
   async _getScores() {
@@ -45,10 +68,15 @@ export class ScoreComponent extends Component {
 
   _displayScores(scores) {
     const table = document.getElementById("scores");
+
+    while (table.children[1]) {
+      table.removeChild(table.children[1]);
+    }
+
     let i = 1;
     scores.forEach((score) => {
       const row = document.createElement("thead");
-      row.innerHTML = `<tr><td>${i}</td><td>${score.name}</td><td>${score.time.toFixed(2)}</td><td>${score.size.toFixed(2)}</td></tr>`;
+      row.innerHTML = `<tr><td>${i}</td><td>${score.name}</td><td>${score.time.toFixed(2)}</td><td>${score.size}</td></tr>`;
       table.appendChild(row);
       i++;
     });
